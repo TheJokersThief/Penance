2c85ff59e43f0f1d0cce0e477c5aa324848e37e3
Evan Smith <esincork@gmail.com>
2016-02-21 16:23:55 +0000

    Look at this wonderful list! BOW BEFORE IT!

--- resources/views/lists/show.blade.php
+++ resources/views/lists/show.blade.php
@@ -4,23 +4,62 @@
 @section('body-class') show-list @endsection
 @section('wrapper-class') valign-wrapper @endsection
 
+@section('extra-js')
+    <script type="text/javascript">
+        $(document).ready( function(){
+            initTaskList( '{{ Crypt::encrypt($list->id) }}');
+        });
+    </script>
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col s12 m8 offset-m2 l6 offset-l3">
             <div class="card">
                 <div class="card-content">
-                    <div class="card-title">{{ $list->title }}</div>     
+                    <div class="card-title">{{ $list->title }}
+                        <a class="waves-effect waves-light chip right green lighten-2" id="show-done" data-bind="click: toggleShowDone">Show Done</a>
+                        <a class="waves-effect waves-light chip right red lighten-2" id="hide-done" data-bind="click: toggleShowDone">Hide Done</a>
+                    </div>     
+                     @if( $list->global )
+                        <p class="chip"><a href="{{ $url = URL::route('showBySlug', $list->slug) }}">({{ $url }})</a></p>
+                    @else
+                        <p class="chip"><a href="{{ $url = URL::route('showByUserSlug', $list->slug, $list->user->name) }}">(({{ $url }}))</a></p>
+                    @endif
+
                     <ul class="collection">
 
-                        @foreach( $list->tasks as $task )
+                        <!-- ko foreach: tasks -->
                             <li class="collection-item">
-                                Alvin
-                                <a href="#!" class="secondary-content"><i class="material-icons">send</i></a>
+                                <div class="col s2 ">
+                                    <!-- ko if: (done == 1) -->
+                                        <input type="checkbox" class="filled-in" data-bind="attr: {id:id}, event: {change: $parent.updateTask}, checkedValue: done, checked: done" />
+                                    <!-- /ko -->
+
+                                    <!-- ko if: (done == 0) -->
+                                        <input type="checkbox" class="filled-in" data-bind="attr: {id:id}, event: {change: $parent.updateTask}" />
+                                    <!-- /ko -->
+
+                                    <label  data-bind="attr: {for:id}"></label>
+                                </div>
+                                <div class="col s10">
+                                    <textarea data-bind="value: description, event: {mouseup: $parent.stripLines, change: $parent.updateTask}" rows="1" ></textarea>
+                                    <!-- <p data-bind="text: description"></p> -->
+                                </div>
                             </li>
-                        @endforeach
+                        <!-- /ko -->
+
+                        <li class="loading collection-item progress">
+                          <div class="indeterminate"></div>
+                        </li>
                      
                     </ul>
+
+                    <div class="new-task">
+                        New Task:
+                        <textarea data-bind="event: {mouseup: createTask}" class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="Press enter to add new task"></textarea>
+                    </div>
                 </div>
             </div>
         </div>
